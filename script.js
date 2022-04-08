d7361f0a442302fc6066096b1eaf622b7487c3c3
Teeshk Nader <teeshk.m@gmail.com>
2022-04-08 11:22:08 +0200

    done

--- script.js
+++ script.js
@@ -19,7 +19,66 @@ console.log(
 Höjd på canvas: ${canvas.height}`
 );
 
-function drawPicture() {
-  // Här skriver du funktionen som ritar bilden
+function drawRec() {
+  let a = document.getElementById("canvas");
+  let ctx = a.getContext("2d");
+  ctx.beginPath();
+  ctx.rect(100, 900, 400, 270);
+  ctx.stroke();
 }
-drawPicture();
+drawRec();
+
+function drawTri() {
+  var b = document.getElementById("canvas");
+  var ctx = b.getContext("2d");
+  ctx.beginPath();
+  ctx.moveTo(100, 900);
+  ctx.lineTo(300, 700);
+  ctx.lineTo(500, 900)
+  ctx.stroke();
+}
+drawTri();
+
+function drawRec2() {
+  let a = document.getElementById("canvas");
+  let ctx = a.getContext("2d");
+  ctx.beginPath();
+  ctx.rect(1050, 900, 400, 270);
+  ctx.stroke();
+}
+drawRec2();
+
+function drawTri2() {
+  var b = document.getElementById("canvas");
+  var ctx = b.getContext("2d");
+  ctx.beginPath();
+  ctx.moveTo(1050, 900);
+  ctx.lineTo(1250, 700);
+  ctx.lineTo(1450, 900)
+  ctx.stroke();
+}
+drawTri2();
+
+
+function drawRecTree() {
+  let c = document.getElementById("canvas");
+  let ctx = c.getContext("2d");
+  ctx.beginPath();
+  ctx.rect(735, 970, 100, 200);
+  ctx.fillStyle = "brown";
+  ctx.fill();
+}
+drawRecTree();
+
+function drawCirTree() {
+  let d = document.getElementById("canvas");
+  let ctx = d.getContext("2d");
+  ctx.beginPath();
+  ctx.ellipse(785, 870, 185, 80, Math.PI / 2, 0.7, 1.78 * Math.PI);
+  ctx.stroke();
+  ctx.fillStyle = "green";
+  ctx.fill();
+
+}
+
+drawCirTree();
