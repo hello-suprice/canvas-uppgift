800413d0838d5aa954ef73acae8768ba53373e1f
Teeshk Nader <teeshk.m@gmail.com>
2022-04-13 16:24:52 +0200

    done 2

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function drawRec() {
   let a = document.getElementById("canvas");
   let ctx = a.getContext("2d");
   ctx.beginPath();
-  ctx.rect(100, 900, 400, 270);
+  ctx.rect(100, 800, 600, 335);
   ctx.stroke();
 }
 drawRec();
@@ -32,9 +32,9 @@ function drawTri() {
   var b = document.getElementById("canvas");
   var ctx = b.getContext("2d");
   ctx.beginPath();
-  ctx.moveTo(100, 900);
-  ctx.lineTo(300, 700);
-  ctx.lineTo(500, 900)
+  ctx.moveTo(100, 800);
+  ctx.lineTo(400, 600);
+  ctx.lineTo(700, 800)
   ctx.stroke();
 }
 drawTri();
@@ -43,7 +43,7 @@ function drawRec2() {
   let a = document.getElementById("canvas");
   let ctx = a.getContext("2d");
   ctx.beginPath();
-  ctx.rect(1050, 900, 400, 270);
+  ctx.rect(1900, 800, 600, 335);
   ctx.stroke();
 }
 drawRec2();
@@ -52,9 +52,9 @@ function drawTri2() {
   var b = document.getElementById("canvas");
   var ctx = b.getContext("2d");
   ctx.beginPath();
-  ctx.moveTo(1050, 900);
-  ctx.lineTo(1250, 700);
-  ctx.lineTo(1450, 900)
+  ctx.moveTo(1900, 800);
+  ctx.lineTo(2200, 600);
+  ctx.lineTo(2500, 800)
   ctx.stroke();
 }
 drawTri2();
@@ -64,7 +64,7 @@ function drawRecTree() {
   let c = document.getElementById("canvas");
   let ctx = c.getContext("2d");
   ctx.beginPath();
-  ctx.rect(735, 970, 100, 200);
+  ctx.rect(1235, 910, 190, 227);
   ctx.fillStyle = "brown";
   ctx.fill();
 }
@@ -74,7 +74,7 @@ function drawCirTree() {
   let d = document.getElementById("canvas");
   let ctx = d.getContext("2d");
   ctx.beginPath();
-  ctx.ellipse(785, 870, 185, 80, Math.PI / 2, 0.7, 1.78 * Math.PI);
+  ctx.ellipse(1330, 790, 220, 150, Math.PI / 2, 0.7, 1.78 * Math.PI);
   ctx.stroke();
   ctx.fillStyle = "green";
   ctx.fill();
